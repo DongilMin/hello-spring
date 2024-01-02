c783fe3a96f01cc87050773c75649fd884fddaff
MIn <tasabi0826@gmail.com>
2024-01-03 07:11:22 +0900

    study

--- src/test/java/hello/hellospring/repository/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/repository/service/MemberServiceTest.java
@@ -38,13 +38,11 @@ class MemberServiceTest {
 
     @Test
     public void DuplicatememberExceptions(){
-        //given
             Member member1 = new Member();
             member1.setName("spring");
 
             Member member2 = new Member();
             member2.setName("spring");
-        //when
             memberService.join(member1);
             IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
             assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
@@ -54,7 +52,6 @@ class MemberServiceTest {
             } catch(IllegalStateException e){
                 assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
             }*/
-        //then
     }
 
     @Test
